7d127f92d26f0b3dfe79788d249017dcaf613563
Juha Kuikka <juha.kuikka@synapse.com>
2017-02-08 18:43:04 -0800

    Add HFP header

---
+++ src/hfp-codecs.h
@@ -0,0 +1,17 @@
+/*
+ * BlueALSA - io.c
+ * Copyright (c) 2017 Juha Kuikka
+ *
+ * This file is a part of bluez-alsa.
+ *
+ * This project is licensed under the terms of the MIT license.
+ *
+ */
+
+#ifndef BLUEALSA_HFP_CODECS_H_
+#define BLUEALSA_HFP_CODECS_H_
+
+#define SCO_CODEC_CVSD		1
+#define SCO_CODEC_MSBC		2
+
+#endif /* BLUEALSA_HFP_CODECS_H_ */
